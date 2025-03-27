bd1b78246f77593df2372f7e087fbcc14ff038dc
YYlab <sxfF9DUbbvyY4sQ>
2025-03-27 17:32:00 +0500

    1. config as file 2. display start time 3. display popular

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 logs_filename='./app.log'
-logs_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+logs_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
 img_cache_folder_name = 'cache_img'
 srt_cache_folder_name = 'cache_srt'
